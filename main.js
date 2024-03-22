3d6f8b727d6888828661b34f59269ac20f210b1e
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-03-22 17:50:00 +0500

    lisson9

---
+++ main.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('.menu')
+const nav = document.querySelector('nav')
+const logo = document.querySelector('.btn2')
+// const navlogo = document.querySelector('.nav-logo')
+
+
+menu.addEventListener('click', ()=>{
+    nav.classList.toggle('gogle')
+})
+logo.addEventListener('click', ()=>{
+    nav.classList.toggle('gogle')
+})

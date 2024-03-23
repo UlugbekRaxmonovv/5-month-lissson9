83039121ad93aec15f8ab837f44d7d983720b941
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-03-23 11:15:38 +0500

    main

--- main.js
+++ main.js
@@ -10,3 +10,78 @@ menu.addEventListener('click', ()=>{
 logo.addEventListener('click', ()=>{
     nav.classList.toggle('gogle')
 })
+
+
+const section =document.querySelector('.CardAll')
+const loader  =document.querySelector('.loader')
+const ApiUrl= 'https://fakestoreapi.com/products' 
+
+async function good(api){
+    let son = await fetch(api)
+    son 
+    .json()
+    .then(ris => card(ris))
+    .catch(irr => console.log(irr))
+    .finally(() =>{
+        loader.style.display = 'none'
+    })
+
+}
+good(ApiUrl)
+
+function card(son){
+    console.log(son[1]);
+    let fragmit = document.createDocumentFragment()
+    console.log('ok');
+    son.forEach(products =>{
+        // console.log(products);
+        const card = document.createElement('div')
+        card.classList.add('hammasi')
+        card.innerHTML =`
+        <div class="hammasi">
+            <div class="hammasi-all">
+                <div class="hammasi-row">
+                    <p class="box">Novinka</p>
+                </div>
+                <div class="hammasi-row">
+                <img src="${products.image}" alt="">
+                </div>
+                <div class="hammasi-all">
+                   <img  src="./img/conversation-regular-24.png" alt="">
+                </div>
+            </div>
+            <p class="p-1">${products.title}
+            </p>
+
+            <div class="list">
+                <div class="list-nav">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:yellow"><path d="M5.025 20.775A.998.998 0 0 0 6 22a1 1 0 0 0 .555-.168L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 0 0-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.214 4.107-1.491 6.452zM12 5.429l2.042 4.521.588.047h.001l3.972.315-3.271 2.944-.001.002-.463.416.171.597v.003l1.253 4.385L12 15.798V5.429z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:yellow"><path d="M5.025 20.775A.998.998 0 0 0 6 22a1 1 0 0 0 .555-.168L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 0 0-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.214 4.107-1.491 6.452zM12 5.429l2.042 4.521.588.047h.001l3.972.315-3.271 2.944-.001.002-.463.416.171.597v.003l1.253 4.385L12 15.798V5.429z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:yellow"><path d="M5.025 20.775A.998.998 0 0 0 6 22a1 1 0 0 0 .555-.168L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 0 0-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.214 4.107-1.491 6.452zM12 5.429l2.042 4.521.588.047h.001l3.972.315-3.271 2.944-.001.002-.463.416.171.597v.003l1.253 4.385L12 15.798V5.429z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:yellow"><path d="M5.025 20.775A.998.998 0 0 0 6 22a1 1 0 0 0 .555-.168L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 0 0-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.214 4.107-1.491 6.452zM12 5.429l2.042 4.521.588.047h.001l3.972.315-3.271 2.944-.001.002-.463.416.171.597v.003l1.253 4.385L12 15.798V5.429z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:yellow"><path d="M5.025 20.775A.998.998 0 0 0 6 22a1 1 0 0 0 .555-.168L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 0 0-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.214 4.107-1.491 6.452zM12 5.429l2.042 4.521.588.047h.001l3.972.315-3.271 2.944-.001.002-.463.416.171.597v.003l1.253 4.385L12 15.798V5.429z"></path></svg>
+                </div>
+                <div class="list-nav">
+                    <span class="p">0 otzivov</span>
+                </div>
+            </div>
+            <h1>1 309 000</h1>
+
+            <div class="all">
+                <div class="all-list">
+                    <div class="al1">Kupite sychas</div>
+                </div>
+                <div class="all-list">
+                    <div class="al2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: white"><circle cx="10.5" cy="19.5" r="1.5"></circle><circle cx="17.5" cy="19.5" r="1.5"></circle><path d="M21 7H7.334L6.18 4.23A1.995 1.995 0 0 0 4.333 3H2v2h2.334l4.743 11.385c.155.372.52.615.923.615h8c.417 0 .79-.259.937-.648l3-8A1.003 1.003 0 0 0 21 7zm-4 6h-2v2h-2v-2h-2v-2h2V9h2v2h2v2z"></path></svg></div>
+                </div>
+            </div>
+        </div>
+        `
+        fragmit.appendChild(card)
+        
+    });
+    section.appendChild(fragmit)
+}
+
+
+
